e417380935f1b67291ef542238c0dd8d7f96842b
Lucas Peixoto <lspeixotodev@gmail.com>
2024-07-02 06:43:10 -0300

    BillList component mobile view updates

--- src/app/pages/bill/bill-list/bill-list-actions/bill-list-actions.component.ts
+++ src/app/pages/bill/bill-list/bill-list-actions/bill-list-actions.component.ts
@@ -13,33 +13,46 @@ import { VDividerComponent } from '@shared/components/divider';
   standalone: true,
   imports: [MatButton, MatIcon, VDividerComponent, MatIconButton],
   template: `
-    <div class="px-10 h-16">
+    <div class="px-10 h-16 border-2 border-cyan-700">
       <div
-        class="border-b dark:border-b-inverse h-full flex items-center justify-between">
-        <div class="flex items-center gap-7">
-          <div class="text-lg font-bold">Bills</div>
-          <app-v-divider class="my-3 h-8"></app-v-divider>
-          <div class="flex items-center gap-4">
+        class="border-b dark:border-b-inverse h-full w-full flex items-center justify-between">
+        <div class="flex items-center gap-1 md:gap-7">
+          <div class="text-lg font-bold hidden md:block">Bills</div>
+          <div class="hidden md:block">
+            <app-v-divider class="my-3 h-8"></app-v-divider>
+          </div>
+          <div class="hidden sm:block items-center sm:gap-4">
             <button mat-stroked-button>
-              <mat-icon class="font-icon">download</mat-icon>Download
+              <mat-icon class="font-icon">download</mat-icon>
+              Download
+            </button>
+          </div>
+          <div class="block sm:hidden items-center gap-1">
+            <button mat-mini-fab>
+              <mat-icon class="font-icon text-cyan-500">download</mat-icon>
             </button>
           </div>
         </div>
-        <div class="my-4 pl-10 w-full mx-4 flex items-center justify-end">
+        <div class="my-4 w-full mx-4 flex items-center justify-end">
           <mat-icon
             class="text-neutral-500 dart:text-neutral-300 font-icon search-icon relative left-8"
             >search</mat-icon
           >
           <input
             type="text"
-            class="pl-10 outline-none placeholder-neutral-500 dark:placeholder-neutral-300 border-none text-md bg-neutral-200 dark:bg-neutral-700 py-3 rounded-lg"
+            class="pl-10 outline-none placeholder-neutral-500 dark:placeholder-neutral-300 border-none text-xs sm:text-md bg-neutral-200 dark:bg-neutral-700 py-3 rounded-lg"
             (keyup)="applyBillsFilter($event)"
             placeholder="Filter bills..."
             #input />
         </div>
-        <div>
+        <div class="hidden sm:block">
           <button mat-flat-button>
             <mat-icon class="font-icon">add</mat-icon> New
+          </button>
+        </div>
+        <div class="block sm:hidden">
+          <button mat-mini-fab>
+            <mat-icon class="font-icon">add</mat-icon>
           </button>
         </div>
       </div>
